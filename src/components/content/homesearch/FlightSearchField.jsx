78709b71b973dee6e08c16ef5a452869e484574b
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2020-01-06 20:37:03 +0100

    Now loading and searching JSON airport lists dynamically, but there are some bugs...

--- src/components/content/homesearch/FlightSearchField.jsx
+++ src/components/content/homesearch/FlightSearchField.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 
 /* 
     Global variable imports
@@ -12,28 +12,56 @@ import { Input } from 'reactstrap';
 
 
 const FlightSearchField = ({identifier, placeholder}) => {
+    //  Context
     const {state, dispatch} = useContext(FlightContext);
+    //  State - current loaded IATA db & search query
+    const [currentQuery, setCurrentQuery] = useState('');
+    const [iataDB, setIataDB] = useState({});
  
-    //  * Submit search query to context when typing
+    //  * Update search query to context when typing
     const setQuery = (inputQuery) => {
-        //  Conditional dynamic importing of alphabetical named airport json lists
-        let db = {};
-            
-        const test = () => {
-            if (inputQuery.length > 2) {
-                db = require(`../../../db_IATA/${inputQuery[0].toUpperCase()}_airports.json`);
-                return db;
-            } else {
-                return 'No db imported yet';
-            }
-        }
-        
-        console.log('Current db: ', test());
+        setCurrentQuery(inputQuery);
+    }
 
+    //  Conditional importing of alphabetical named airport lists
+    /* 
+        - Problem: Json files are named based on IATA code, so if someone searches
+        for an airport location starting with 'Bah', but IATA code of desired airport starts with 'Xxx',
+        the airport will not be found...
+    */
+    const loadDBCondition = (inputQuery) => {
+        if (inputQuery.length > 2) {
+            if(Object.keys(iataDB)[0] !== inputQuery[0].toUpperCase() || Object.keys(iataDB)[0] === undefined) {
+                setIataDB(require(`../../../db_IATA/${inputQuery[0].toUpperCase()}_airports.json`));
+                console.log('Load DB status','New DB loaded');
+            } else {
+                console.log('Load DB status','No need to load another DB');
+            }
+        } else {
+            console.log('Load DB status','DB will import after > 2 char query length');
+        }
+    }
+
+    //  Handle query change
+    const handleQueryChange = (inputQuery) => {
+        setQuery(inputQuery);
+        loadDBCondition(inputQuery);
+    }
+
+    //  Handle submission:
+    const handleSubmission = (query) => {
+        const iata = iataDB.find((q)=>{
+            return q.location.includes(query) === true;
+        });
+
+        console.log(iata);
+
+        /* 
         dispatch({
-            target: identifier,
-            payload: inputQuery
-        })
+                    target: identifier,
+                    payload: iata
+                }) 
+        */
     }
     
     return (
@@ -41,7 +69,12 @@ const FlightSearchField = ({identifier, placeholder}) => {
         type="text" 
         identifier={identifier} 
         placeholder={placeholder}
-        onChange={(e)=>{setQuery(e.target.value)}}
+        onChange={(e)=>{handleQueryChange(e.target.value)}}
+        onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmission(currentQuery);
+            }
+          }}
         />
     )
 }
